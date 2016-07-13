01d5867bff32f0d579a4f75165d946421ab1f671
Guilherme Mattar Bastos <guilhermembastos@gmail.com>
2016-07-13 18:06:29 -0300

    Fix lint errors.

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,14 @@ var nunjucksRender  = require('gulp-nunjucks-render'),
     gulp            = require('gulp'),
     sass            = require('gulp-sass'),
     data            = require('gulp-data'),
-    imagemin        = require('gulp-imagemin'),
-    cache           = require('gulp-cache'),
+    // imagemin        = require('gulp-imagemin'),
+    // cache           = require('gulp-cache'),
     del             = require('del');
 
 // Clean Dist
 gulp.task('clean:dist', function() {
   return del.sync('dist');
-})
+});
 
 // Browser Sync
 gulp.task('browserSync', function() {
@@ -18,32 +18,31 @@ gulp.task('browserSync', function() {
     server: {
       baseDir: 'public'
     },
-  })
-})
+  });
+});
 
 // Sass
 gulp.task('sass', function() {
-  return gulp.src('source/sass/**/*.scss') // Gets all files ending with .scss in source/sass
+  // Gets all files ending with .scss in source/sass
+  return gulp.src('source/sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('public'))
-    .pipe(browserSync.reload({
-      stream: true
-    }))
+    .pipe(browserSync.reload({ stream: true }));
 });
 
-gulp.task('vendor', function(){
+gulp.task('vendor', function() {
   return gulp.src('source/static/vendor/**/*')
-  .pipe(gulp.dest('public/vendor'))
+    .pipe(gulp.dest('public/vendor'));
 });
 
-gulp.task('image-dev', function(){
+gulp.task('image-dev', function() {
   return gulp.src('source/static/images/**/*')
-  .pipe(gulp.dest('public/images'))
+    .pipe(gulp.dest('public/images'));
 });
 
-gulp.task('js-dev', function(){
+gulp.task('js-dev', function() {
   return gulp.src('source/static/*.js')
-  .pipe(gulp.dest('public'))
+    .pipe(gulp.dest('public'));
 });
 
 // Nunjucks
@@ -52,25 +51,24 @@ gulp.task('nunjucks', function() {
 
   // Gets .html and .nunjucks files in pages
   return gulp.src('source/templates/**/[^_]*.+(html|nunjucks)')
-  // Adding data to Nunjucks
-  .pipe(data(function() {
-    return require('./source/data/data.json')
-  }))
-  // Renders template with nunjucks
-  .pipe(nunjucksRender())
-  // output files in app folder
-  .pipe(gulp.dest('public'))
-  .pipe(browserSync.reload({
-      stream: true
-    }))
+    // Adding data to Nunjucks
+    .pipe(data(function() { return require('./source/data/data.json'); }))
+    // Renders template with nunjucks
+    .pipe(nunjucksRender({path: 'source/templates'}))
+    // output files in app folder
+    .pipe(gulp.dest('public'))
+    .pipe(browserSync.reload({ stream: true }));
 });
 
-
-gulp.task('watch', ['browserSync', 'sass', 'nunjucks', 'image-dev', 'vendor', 'js-dev', 'image-dev'], function (){
-  gulp.watch('source/sass/**/*.scss', ['sass']); 
-  gulp.watch('source/static/scripts.js', ['js-dev']); 
-  gulp.watch('source/templates/**/*.html', ['nunjucks']); 
-  // Other watchers
-});
+gulp.task(
+  'watch',
+  ['browserSync', 'sass', 'nunjucks', 'image-dev', 'vendor', 'js-dev'],
+  function() {
+    gulp.watch('source/sass/**/*.scss', ['sass']);
+    gulp.watch('source/static/scripts.js', ['js-dev']);
+    gulp.watch('source/templates/**/*.html', ['nunjucks']);
+    // Other watchers
+  }
+);
 
 gulp.task('default', ['watch']);
--- source/static/scripts.js
+++ source/static/scripts.js
@@ -1,32 +1,29 @@
 $(document).ready(function() {
+    // Smooth Scrolling Function
+    $(function() {
+        $('a[href*=#]:not([href=#])').click(function() {
+            if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+                || location.hostname == this.hostname) {
 
-// Smooth Scrolling Function
-$(function() {
-    $('a[href*=#]:not([href=#])').click(function() {
-        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
-            || location.hostname == this.hostname) {
+                var tgt = $(this.hash);
+                tgt = tgt.length ? tgt : $('[name=' + this.hash.slice(1) +']');
 
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-        if (target.length) {
-            $('html,body').animate({
-              scrollTop: target.offset().top
-            }, 1000);
-            return false;
+                if (tgt.length) {
+                    $('html, body').animate({
+                        scrollTop: tgt.offset().top
+                    }, 1000);
+
+                    return false;
+                }
             }
-        }
+        });
     });
-});
 
+    $('.m-prev').click(function() {
+        $(this).closest('.e-testimonial-item').slick('slickPrev');
+    });
 
-$('.m-prev').click(function() {
-    $(this).closest('.e-testimonial-item').slick('slickPrev');
-});
-
-$('.m-next').click(function() {
-    $(this).closest('.e-testimonial-item').slick('slickNext');
-});
-
-
+    $('.m-next').click(function() {
+        $(this).closest('.e-testimonial-item').slick('slickNext');
+    });
 }); // doc.ready
-
