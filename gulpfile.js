b2f81369eab254fb4d90a8f743c9bc0b5388b4bf
Guilherme Mattar Bastos <guilhermembastos@gmail.com>
2016-07-19 20:36:52 -0300

    Improve Gulp workflow.

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,17 @@
-var nunjucksRender  = require('gulp-nunjucks-render'),
-    browserSync     = require('browser-sync'),
-    sourcemaps      = require('gulp-sourcemaps'),
-    imagemin        = require('gulp-imagemin'),
-    uglify          = require('gulp-uglify'),
-    shell           = require('gulp-shell'),
+var del             = require('del'),
     gulp            = require('gulp'),
     sass            = require('gulp-sass'),
-    del             = require('del');
-    // cache           = require('gulp-cache'),
+    shell           = require('gulp-shell'),
+    uglify          = require('gulp-uglify'),
+    imagemin        = require('gulp-imagemin'),
+    sourcemaps      = require('gulp-sourcemaps'),
+    browserSync     = require('browser-sync'),
+    runSequence     = require('run-sequence').use(gulp),
+    nunjucksRender  = require('gulp-nunjucks-render');
 
 // Clean Dist
-gulp.task('clean:public', function () {
-  return del.sync('public');
+gulp.task('clean', function () {
+  return del(['public']);
 });
 
 // Browser Sync
@@ -29,7 +29,7 @@ gulp.task('sass', function () {
   return gulp.src('source/sass/**/*.scss')
     .pipe(sourcemaps.init())
     .pipe(sass({ outputStyle: 'compressed' })).on('error', sass.logError)
-    .pipe(sourcemaps.write())
+    .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest('public/styles'))
     .pipe(browserSync.reload({ stream: true }));
 });
@@ -64,21 +64,33 @@ gulp.task('nunjucks', function () {
     .pipe(browserSync.reload({ stream: true }));
 });
 
-gulp.task('deploy', ['sass', 'js', 'image', 'nunjucks', 'vendor'], shell.task([
-  'git subtree push --prefix public origin gh-pages'
-]));
-
 gulp.task(
-  'watch',
-  ['browserSync', 'sass', 'js', 'image', 'nunjucks', 'vendor'],
-  function () {
-    gulp.watch('source/static/**/*.js', ['js']);
-    gulp.watch('source/sass/**/*.scss', ['sass']);
-    gulp.watch('source/templates/**/*.html', ['nunjucks']);
-    gulp.watch('source/static/vendor/**/*.js', ['vendor']);
-    gulp.watch('source/static/vendorimages/**/*', ['images']);
-    // Other watchers
-  }
+  'git-push',
+  shell.task(['git subtree push --prefix public origin gh-pages'])
 );
 
-gulp.task('default', ['watch']);
+gulp.task('deploy', function (callback) {
+  runSequence(
+    'clean',
+    ['sass', 'js', 'image', 'nunjucks', 'vendor'],
+    'git-push',
+    callback
+  );
+});
+
+gulp.task('watch', function () {
+  gulp.watch('source/static/**/*.js', ['js']);
+  gulp.watch('source/sass/**/*.scss', ['sass']);
+  gulp.watch('source/templates/**/*.html', ['nunjucks']);
+  gulp.watch('source/static/vendor/**/*.js', ['vendor']);
+  gulp.watch('source/static/vendorimages/**/*', ['images']);
+});
+
+gulp.task('default', function (callback) {
+  runSequence(
+    'clean',
+    ['sass', 'js', 'image', 'nunjucks', 'vendor'],
+    ['browserSync', 'watch'],
+    callback
+  );
+});
