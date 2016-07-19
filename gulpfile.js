6aa865898619f138cf2beb2c8c0d70910baf0840
Guilherme Mattar Bastos <guilhermembastos@gmail.com>
2016-07-19 19:52:55 -0300

    Include minifiers.

--- gulpfile.js
+++ gulpfile.js
@@ -1,78 +1,84 @@
 var nunjucksRender  = require('gulp-nunjucks-render'),
     browserSync     = require('browser-sync'),
+    sourcemaps      = require('gulp-sourcemaps'),
+    imagemin        = require('gulp-imagemin'),
+    uglify          = require('gulp-uglify'),
+    shell           = require('gulp-shell'),
     gulp            = require('gulp'),
     sass            = require('gulp-sass'),
-    data            = require('gulp-data'),
-    // imagemin        = require('gulp-imagemin'),
-    // cache           = require('gulp-cache'),
     del             = require('del');
+    // cache           = require('gulp-cache'),
 
 // Clean Dist
-gulp.task('clean:dist', function() {
-  return del.sync('dist');
+gulp.task('clean:public', function () {
+  return del.sync('public');
 });
 
 // Browser Sync
-gulp.task('browserSync', function() {
+gulp.task('browserSync', function () {
   browserSync({
     server: {
       baseDir: 'public'
-    },
+    }
   });
 });
 
 // Sass
-gulp.task('sass', function() {
+gulp.task('sass', function () {
   // Gets all files ending with .scss in source/sass
   return gulp.src('source/sass/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
+    .pipe(sourcemaps.init())
+    .pipe(sass({ outputStyle: 'compressed' })).on('error', sass.logError)
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest('public/styles'))
     .pipe(browserSync.reload({ stream: true }));
 });
 
-gulp.task('vendor', function() {
+gulp.task('vendor', function () {
   return gulp.src('source/static/vendor/**/*')
     .pipe(gulp.dest('public/vendor'));
 });
 
-gulp.task('image-dev', function() {
+gulp.task('image', function () {
   return gulp.src('source/static/images/**/*')
+    .pipe(imagemin())
     .pipe(gulp.dest('public/images'));
 });
 
-gulp.task('js-dev', function() {
-  return gulp.src('source/static/*.js')
+gulp.task('js', function () {
+  return gulp.src('source/static/**/*.js')
+    .pipe(uglify())
     .pipe(gulp.dest('public'));
 });
 
 // Nunjucks
-gulp.task('nunjucks', function() {
+gulp.task('nunjucks', function () {
   nunjucksRender.nunjucks.configure(['source/templates/']);
 
   // Gets .html and .nunjucks files in pages
-  return gulp.src('source/templates/**/[^_]*.+(html|nunjucks)')
+  return gulp.src('source/templates/**/[^_]*.html')
     // Renders template with nunjucks
-    .pipe(nunjucksRender({path: 'source/templates'}))
+    .pipe(nunjucksRender({ path: 'source/templates' }))
     // output files in app folder
     .pipe(gulp.dest('public'))
     .pipe(browserSync.reload({ stream: true }));
 });
 
+gulp.task('deploy', ['sass', 'js', 'image', 'nunjucks', 'vendor'], shell.task([
+  'git subtree push --prefix public origin gh-pages'
+]));
+
 gulp.task(
   'watch',
-  ['browserSync', 'sass', 'nunjucks', 'image-dev', 'vendor', 'js-dev'],
-  function() {
+  ['browserSync', 'sass', 'js', 'image', 'nunjucks', 'vendor'],
+  function () {
+    gulp.watch('source/static/**/*.js', ['js']);
     gulp.watch('source/sass/**/*.scss', ['sass']);
-    gulp.watch('source/static/scripts.js', ['js-dev']);
     gulp.watch('source/templates/**/*.html', ['nunjucks']);
+    gulp.watch('source/static/vendor/**/*.js', ['vendor']);
+    gulp.watch('source/static/vendorimages/**/*', ['images']);
     // Other watchers
   }
 );
 
 gulp.task('default', ['watch']);
-
-
-
-
-
-
--- source/static/scripts.js
+++ source/static/scripts.js
@@ -1,67 +1,63 @@
-$(document).ready(function() {
+$(document).ready(function () {
     // Smooth Scrolling Function
-    $(function() {
-        $('a[href*=#]:not([href=#])').click(function() {
-            if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
-                || location.hostname == this.hostname) {
+    $('a[href*=#]:not([href=#])').click(function () {
+        var $targ = $(this.hash),
+            host1 = this.hostname,
+            host2 = location.hostname,
+            path1 = this.pathname.replace(/^\//, ''),
+            path2 = location.pathname.replace(/^\//, '');
 
-                var tgt = $(this.hash);
-                tgt = tgt.length ? tgt : $('[name=' + this.hash.slice(1) +']');
+        if (!$targ.length) {
+            $targ = $('[name=' + this.hash.slice(1) + ']');
+        }
 
-                if (tgt.length) {
-                    $('html, body').animate({
-                        scrollTop: tgt.offset().top
-                    }, 1000);
+        if ($targ.length && (host1 === host2 || path1 === path2)) {
+            $('html, body').animate({ scrollTop: $targ.offset().top }, 1000);
 
-                    return false;
-                }
-            }
-        });
+            return false;
+        }
+
+        return true;
     });
 
-
     // Modal Click Behavior
-    $('.js-open-modal').click(function() {
+    $('.js-open-modal').click(function () {
         $('.js-target-modal').addClass('js-active');
         $('#overlay').addClass('js-active');
         $('body').addClass('js-body-modal-active');
     });
 
-    $('.js-close-modal').click(function() {
+    $('.js-close-modal').click(function () {
         $('.js-target-modal').removeClass('js-active');
         $('#overlay').removeClass('js-active');
         $('body').removeClass('js-body-modal-active');
     });
 
-
     // Sticky Click Behavior
-    $('.js-close-sticky').click(function() {
+    $('.js-close-sticky').click(function () {
         $('.js-target-sticky').removeClass('js-active');
     });
 
     // Search Click Behavior
-    $('.js-trigger-search').click(function(e) {
+    $('.js-trigger-search').click(function (e) {
         e.preventDefault();
         $(this).parent().addClass('js-active');
         $('#overlay').addClass('js-active');
     });
 
     // Main Menu Click Behavior
-    $('.js-trigger-menu').click(function(e) {
+    $('.js-trigger-menu').click(function (e) {
         $(this).next().addClass('js-active-menu');
         $('#overlay').addClass('js-active');
     });
 
-
     // General Click Behavior for Overlay
-    $('#overlay').click(function() {
+    $('#overlay').click(function () {
         $('.js-active').removeClass('js-active');
         $('.js-active-menu').removeClass('js-active-menu');
     });
 
-
     // Slider
-
     $('.slider').slick({
         arrows: true,
         draggable: false,
@@ -72,11 +68,9 @@ $(document).ready(function() {
             {
                 breakpoint: 800,
                 settings: {
-                    draggable: true,
+                    draggable: true
                 }
             }
         ]
     });
-
-
 }); // doc.ready
